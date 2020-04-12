1a39d02470ea4e358ac2745ae6e9a4574b5fd854
Corentin Kirscher <corentin.kirscher@etu.univ-amu.fr>
2020-04-12 22:05:19 +0200

    Add fragment, and some layouts

--- app/src/main/java/com/example/project_smart_city/MainActivity.java
+++ app/src/main/java/com/example/project_smart_city/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.project_smart_city;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.connexion_layout);
+        setContentView(R.layout.main_activity);
+
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 }

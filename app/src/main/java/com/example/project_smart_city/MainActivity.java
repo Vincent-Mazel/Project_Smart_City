6ca7ecc63ccb34b3e9939968175107486573d24e
Corentin Kirscher <corentin.kirscher@etu.univ-amu.fr>
2020-04-13 16:59:04 +0200

    Add Network layout fragments

--- app/src/main/java/com/example/project_smart_city/MainActivity.java
+++ app/src/main/java/com/example/project_smart_city/MainActivity.java
@@ -3,17 +3,21 @@ package com.example.project_smart_city;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.widget.Toast;
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
     }
 }
+

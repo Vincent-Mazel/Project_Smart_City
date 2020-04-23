bc1ea74d1adf92dc91d5ef23538cef67a92c0197
Corentin Kirscher <corentin.kirscher@etu.univ-amu.fr>
2020-04-23 13:38:42 +0200

    Add choice fragment & linked it to profil one.

--- app/src/main/java/com/example/project_smart_city/MainActivity.java
+++ app/src/main/java/com/example/project_smart_city/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.project_smart_city;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ScrollView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -10,25 +12,43 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    private static ScrollView scrollView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getSupportActionBar().hide();
+
+        //getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        scrollView = findViewById(R.id.scroll_main);
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
 
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home,R.id.navigation_shopping , R.id.navigation_network, R.id.navigation_profil).
-                build();
+               build();
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+
     }
 
+    public void onClickBot(View view){
+        scrollView.post(new Runnable() { public void run() { scrollView.fullScroll(View.FOCUS_DOWN); } });
+    }
 
+    public void onClickTop(View view){
+        scrollView.post(new Runnable() { public void run() { scrollView.fullScroll(View.FOCUS_UP); } });
+    }
+
+    public static ScrollView getScrollView(){
+        return scrollView;
+    }
 }
 
--- app/src/main/java/com/example/project_smart_city/ui/ProfilFragment/ProfilFragment.java
+++ app/src/main/java/com/example/project_smart_city/ui/ProfilFragment/ProfilFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,37 +15,59 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.project_smart_city.R;
+import com.example.project_smart_city.ui.ChoicesFragment.ChoiceFragment;
+
+import java.util.Objects;
 
 
 public class ProfilFragment extends Fragment implements View.OnClickListener{
 
     private ProfilViewModel profilViewModel;
-    public Button buttonToPref;
+    private Button buttonToPref;
+    private ScrollView sc;
+    private Button btnSaveChenges;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         profilViewModel =
                 ViewModelProviders.of(this).get(ProfilViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profil, container, false);
+
+
+
         buttonToPref = root.findViewById(R.id.gererPreference);
         buttonToPref.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Implémenter création new frag sur le choix des preférences
-                System.out.println("it works !");
+                ChoiceFragment nextFrag= new ChoiceFragment();
+                Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_profil, nextFrag, "findThisFragment")
+                        .addToBackStack(null)
+                        .commit();
+
             }
         });
 
-        final TextView textView = root.findViewById(R.id.tilte_profil);
+        btnSaveChenges = root.findViewById(R.id.profil_btnSaveChange);
+        btnSaveChenges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("choix saved");
+
+            }
+        });
+        TextView title = root.findViewById(R.id.tilte_profil);
         profilViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                title.setText(s);
             }
         });
 
         return root;
     }
+
 
     @Override
     public void onClick(View view) {

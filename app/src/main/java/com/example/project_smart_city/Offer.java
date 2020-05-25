29432d5e04602bbcb1a5101e81593feaed5f332e
Corentin Kirscher <corentin.kirscher@etu.univ-amu.fr>
2020-05-25 13:53:46 +0200

    add Offer & Shop classes

---
+++ app/src/main/java/com/example/project_smart_city/Offer.java
@@ -0,0 +1,65 @@
+package com.example.project_smart_city;
+
+public class Offer {
+    private int id;
+    private String name;
+    private int shop_id;
+    private byte[] picture;
+    private double price;
+    private String description;
+
+    public Offer(String name, int shop_id, double price, String description) {
+        this.name = name;
+        this.shop_id = shop_id;
+        this.price = price;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getShop_id() {
+        return shop_id;
+    }
+
+    public void setShop_id(int shop_id) {
+        this.shop_id = shop_id;
+    }
+
+    public byte[] getPicture() {
+        return picture;
+    }
+
+    public void setPicture(byte[] picture) {
+        this.picture = picture;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ app/src/main/java/com/example/project_smart_city/Shop.java
@@ -0,0 +1,89 @@
+package com.example.project_smart_city;
+
+import java.util.ArrayList;
+
+public class Shop {
+    private int id;
+    private double latitude, longitude;
+    private String name;
+    private String description;
+    private byte[] picture;
+    private ArrayList<Offer> offers;
+    private String sector;
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    public void addOffer(Offer offer){
+        this.offers.add(offer);
+    }
+
+    public Shop(double latitude, double longitude, String name, String description, String sector) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.name = name;
+        this.description = description;
+        this.sector = sector;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public byte[] getPicture() {
+        return picture;
+    }
+
+    public void setPicture(byte[] picture) {
+        this.picture = picture;
+    }
+
+    public ArrayList<Offer> getOffers() {
+        return offers;
+    }
+
+    public void setOffers(ArrayList<Offer> offers) {
+        this.offers = offers;
+    }
+}
